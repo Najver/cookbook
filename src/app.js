9d03914ea5ece7f1c3b58c8a22ea456048d01ff0
Najver <pigino17333@seznam.cz>
2024-11-06 14:20:15 +0100

    sql conncetd

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 const app = express();
 const PORT = 3000;
+
+// Nastavení připojení k databázi
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Maminka007',
+  database: 'cookbook'
+});
+
+// Připojení k databázi
+db.connect(err => {
+  if (err) {
+    console.error('Chyba při připojování k databázi:', err);
+    return;
+  }
+  console.log('Připojeno k databázi');
+});
 
 // Servírujeme statické soubory ze složky `public`
 app.use(express.static(path.join(__dirname, '../public')));
@@ -21,6 +39,18 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '../html/register.html'));
+});
+
+// Přidáme API endpoint pro získání dat z databáze
+app.get('/api/get-data', (req, res) => {
+    db.query('SELECT * FROM users', (err, result) => {
+        if (err) {
+            console.error('Chyba při získávání dat:', err);
+            res.status(500).send('Chyba serveru');
+            return;
+        }
+        res.json(result);
+    });
 });
 
 // Spuštění serveru
